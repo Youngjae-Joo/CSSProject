969ae2861d3504e252fba56b80c7d09a0ce3f350
akdsksdyd <kakalote4@gmail.com>
2023-01-10 19:18:48 +0900

    01-10 jaewon

--- mainPage/mainJS.js
+++ mainPage/mainJS.js
@@ -1,2 +1,7 @@
 var titleBox = document.querySelector(".titleBox");
 var contentBox = document.querySelector(".contentBox");
+
+titleBox.onclick = function(){
+    if(event.target.tagName !== 'INPUT')return;
+    
+}
