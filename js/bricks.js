5e41f09844a9b0723c99747b14e41a96b7ec2057
sschoi1994 <sschoi1994@naver.com>
2023-01-10 18:51:45 +0900

    finish

--- js/bricks.js
+++ js/bricks.js
@@ -27,26 +27,39 @@ var rightPressed = false;
 var leftPressed = false;
 
 //벽돌변수
-var brickRowCount = 5;      //열
-var brickColumnCount = 8;   //행
+var brickRowCount = 8;      //열
+var brickColumnCount = 10;   //행
 var brickWidth = 48;        //넓이
 var brickHeight = 15;       //높이
 var brickPadding = 5;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
+var blockLife = 0;
+
 //점수 생성
 var score = 0;
 
 //사용자 생명 생성
 var lives = 3;
+//난이도 생성
+var level = 1;
+if (level = localStorage.getItem("level") == null) {
+    level = 1;
+} else {
+    level = localStorage.getItem("level");
+}
 
 //벽돌생성
+
 var bricks = [];
-for (var c = 0; c < brickColumnCount; c++) {
-    bricks[c] = [];
-    for (var r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 2 };
+function createBricks() {
+    bricks = [];
+    for (var c = 0; c < brickColumnCount; c++) {
+        bricks[c] = [];
+        for (var r = 0; r < brickRowCount; r++) {
+            bricks[c][r] = { x: 0, y: 0, status: level };
+        }
     }
 }
 
@@ -74,18 +87,39 @@ function keyUpHandler(e) {
     }
 }
 
+//게임시작 전 멘트
+function drawText() {
+    ctx.font = "28px Arial";
+    ctx.fillStyle = "balck";
+    ctx.fillText("게임을 시작하려면 시작 버튼을 누르세요.", 45, 220)
+}
+drawText();
+
+
 //벽돌 충돌시 방향전환
 function collisionDetection() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             var b = bricks[c][r];
-            if (b.status >= 1) {
-                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-                    dy = -dy;
-                    b.status--;
-                    score++;
-                    if (score == brickRowCount * brickColumnCount * 2) {
-                        alert("YOU WIN, CONGRATULATIONS!");
+            if( blockLife==0){
+                blockLife = b.status;
+                console.log(blockLife)
+            }
+            if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                dy = -dy;
+                console.log(blockLife)
+                blockLife--;
+                score++;
+                if (score == brickRowCount * brickColumnCount * level) {
+                    alert("YOU WIN, CONGRATULATIONS!");
+                    if (level == 3) document.location.reload();
+                    if (confirm("계속 하실?")) {
+                        level++;
+                        lives++;
+                        localStorage.setItem("level", level);
+                        document.location.reload();
+                        document.location.reload();
+                    } else {
                         document.location.reload();
                     }
                 }
@@ -98,16 +132,16 @@ function collisionDetection() {
 function drawBricks() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
-            if (bricks[c][r].status >= 1) {
+            if (bricks[c][r].status >= level) {
                 var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
                 var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                if (bricks[c][r].status == 2) ctx.fillStyle = "#0095DD";
-                if (bricks[c][r].status == 1) ctx.fillStyle = "#828282";
-
+                if (blockLife == 3) ctx.fillStyle = "#222222";
+                if (blockLife == 2) ctx.fillStyle = "#0095DD";
+                if (blockLife == 1) ctx.fillStyle = "#828282";
                 ctx.fill();
                 ctx.closePath();
             }
@@ -146,6 +180,7 @@ function drawLives() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    createBricks();
     drawBricks();
     drawBall();
     drawPaddle();
@@ -161,34 +196,41 @@ function draw() {
         dy = -dy;
     }
     else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
+        if (x >= paddleX && x <= paddleX + paddleWidth) {
 
-            //속도 조절
-            dy = -dy * (Math.random() + 0.8);
-            if (dy <= -5) dy = -2;
-            //paddle
-            console.log(paddleX)
-            console.log(paddleX + paddleWidth)
-            console.log("")
-            if (x < (paddleX + paddleX + paddleWidth) / 2) {
-                console.log("left");
-                console.log((paddleX + paddleX + paddleWidth) / 2)
-                console.log(x);
-            } else if (x > (paddleX + paddleX + paddleWidth) / 2) {
-                console.log("right");
-                console.log((paddleX + paddleX + paddleWidth) / 2)
-                console.log(x);
+            // //속도 조절
+            // dy = -dy * (Math.random() + 0.8);
+            // if (dy <= -5) dy = -2;
+            // x += dx * Math.round((Math.random() * 2 + 1));
+
+            if (x < paddleX + 20) {
+                if (dx > 0) {
+                    dx = -dx;
+                }
+                dx = -3.5;
+            } else if (x < paddleX + 40) {
+                if (dx > 0) {
+                    dx = -dx;
+                }
+                dx = -2;
             }
-
-
-            x += dx * Math.round((Math.random() * 2 + 1));
-
+            else if (x < paddleX + 60) {
+                if (dx < 0) {
+                    dx = -dx;
+                }
+                dx = 2;
+            } else {
+                if (dx < 0) {
+                    dx = -dx;
+                }
+                dx = 3.5;
+            }
+            dy = -dy;
         } else {
             lives--;
             if (!lives) {
                 alert("GAME OVER");
                 document.location.reload();
-
             }
             else {
                 x = canvas.width / 2;
@@ -216,17 +258,14 @@ var stopBtn = document.querySelector(".stopBtn");
 var exitBtn = document.querySelector(".exitBtn")
 
 startBtn.onclick = function start() {
-
     interval = setInterval(draw, 10);
 }
 stopBtn.onclick = function sto() {
     clearInterval(interval)
 }
 exitBtn.onclick = function exit() {
-    draw();
+    clearInterval(interval)
     canvas.width = canvas.width;
-    // clearInterval(interval)
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx.beginPath();
+    localStorage.clear();
 }
 
